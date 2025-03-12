952e70d9b0ae558d4c681b7ba41466579125defe
MarianaMendanha <mariana.mendanha.mm@gmail.com>
2025-03-12 11:36:27 -0300

    updating frontend axios production url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,12 @@
 // src/api/api.ts
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api"; 
+
+const isDevelopment = import.meta.env.MODE === 'development';
+const baseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD;
 
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
