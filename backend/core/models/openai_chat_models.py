f6e601043fdfea28bdee223df5500198790e4c4f
MarianaMendanha <mariana.mendanha.mm@gmail.com>
2025-05-15 17:05:52 -0300

    fixing chat perplexity task model

--- backend/core/models/openai_chat_models.py
+++ backend/core/models/openai_chat_models.py
@@ -7,7 +7,7 @@ class ChatConversation(models.Model):
     id = models.CharField(max_length=40, primary_key=True, default=uuid.uuid4)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, unique=True, default=None)
+    name = models.CharField(max_length=100, unique=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
